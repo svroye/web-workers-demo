1a87f6a0bba2dc0e0abd3d94b9ba8e77b83accbb
Van Roye <steven.van-roye@sogeti.com>
2017-05-30 21:42:23 +0200

    Use of worker thread

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -43,21 +43,18 @@
     // Hint! This is where you should post messages to the web worker and
     // receive messages from the web worker.
 
-    length = imageData.data.length / 4;
-    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-      r = imageData.data[i * 4 + 0];
-      g = imageData.data[i * 4 + 1];
-      b = imageData.data[i * 4 + 2];
-      a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
-      imageData.data[i * 4 + 0] = pixel[0];
-      imageData.data[i * 4 + 1] = pixel[1];
-      imageData.data[i * 4 + 2] = pixel[2];
-      imageData.data[i * 4 + 3] = pixel[3];
+    var myWorker = new Worker('scripts/worker.js');
+    myWorker.postMessage({'imageData' : imageData, 'type': type});
+   
+    myWorker.onmessage = function(e){
+       toggleButtonsAbledness();
+       var image = e.data;
+       if (image) {
+        return ctx.putImageData(e.data,0,0);
+       }
+       console.log("No manipulated image returned");
     }
-    toggleButtonsAbledness();
-    return ctx.putImageData(imageData, 0, 0);
-  };
+  }
 
   function revertImage() {
     return ctx.putImageData(original, 0, 0);
